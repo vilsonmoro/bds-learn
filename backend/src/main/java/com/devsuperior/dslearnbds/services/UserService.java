6371165ecd75a96d3449a04c9b8f580b3756cb0a
vilsonmoro <vilsonmoro@gmail.com>
2021-02-25 15:11:46 -0300

    OAuth security login testado

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dslearnbds.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+	private static Logger logger =  LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
+		if (user == null) {
+			logger.error("Usuario não encontrado " + username);
+			throw new UsernameNotFoundException("Email não encontrado");
+		}
+		logger.info("Usuario  encontrado " + username);
+		return user;
+	}
+}
