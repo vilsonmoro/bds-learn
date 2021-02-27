19478bfc00a5811df9cbc52d1b53bc6cb17e1959
vilsonmoro <vilsonmoro@gmail.com>
2021-02-27 14:10:31 -0300

    Custom content for user, JPQL QUERY

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
@@ -83,6 +83,15 @@ public class Notification implements Serializable {
 		this.route = route;
 	}
 
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
